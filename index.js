73707e4a410cf5af7c9e3f8cc12beb6e165811e7
gaoya <1975238923@qq.com>
2019-01-30 09:47:29 +0800

    11

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const categoryRouter = require('./routes/admin/category');
 const adminRouter = require('./routes/admin/admin');
 const dishRouter = require('./routes/admin/dish');
 const settingsRouter=require('./routes/admin/settings')
+const tableRouter=require('./routes/admin/table')
 //创建HTTP应用服务器
 var app = express(); 
 app.listen(PORT, ()=>{
@@ -26,4 +27,5 @@ app.use('/admin/category', categoryRouter);
 app.use('/admin', adminRouter);
 app.use('/admin/dish',dishRouter);
 app.use('/admin/settings',settingsRouter);
+app.use('/admin/table',tableRouter);
 
---
+++ routes/admin/table.js
@@ -0,0 +1,19 @@
+/*桌台相关的路由器 */
+const express=require('express')
+const pool=require('../../pool')
+const router=express.Router();
+module.exports=router;
+
+
+/*GET  /admin/table 
+获取所有的桌台信息
+返回数据{tid:'xx',tname：'xx',status:'xx'}
+*/
+router.get('/',(req,res)=>{
+    pool.query('SELECT * FROM xfn_table ORDER BY tid',(err,result)=>{
+        if(err)throw err;
+        res.send(result);
+    })
+})
+
+
